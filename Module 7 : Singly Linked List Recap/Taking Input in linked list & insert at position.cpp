f58dd2aff0a89d46443dfd3083b5218e2e0923a5
Nayem Islam <128284940+zeeSaan25@users.noreply.github.com>
2023-12-11 01:17:09 +0600

    Create Taking Input in linked list & insert at position.cpp
    This input-taking approach and this insertion approach are the final ways to do these operations. We'll use these approaches in the future.

---
+++ Module 7 : Singly Linked List Recap/Taking Input in linked list & insert at position.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+class Node{
+        public:
+        int value;
+        Node* next;
+
+        Node(int value){
+            this->value=value;
+            this->next=NULL;
+        }
+    };
+
+void insert_at_tail(Node *& head, Node *& tail, int val){
+    Node * newNode = new Node(val);
+    if(head == NULL){
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void print_linked_list(Node * head){
+    Node * tmp = head;
+    while(tmp != NULL){
+        cout<<tmp->value<<" ";
+        tmp = tmp->next;
+    }
+    cout<<endl;
+}
+
+void insert_at_head(Node *& head, int val){
+    Node * newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+
+}
+
+int size_counter(Node * head){
+    Node * tmp = head;
+    int count = 0;
+    while(tmp != NULL){
+        count++;
+        tmp = tmp->next;
+    }
+    return count;
+}
+
+void insert_at_position(Node * head, int val, int pos){
+    Node * newNode = new Node(val);
+    Node * tmp = head;
+    if(head == NULL){
+        head = newNode;
+        return;
+    }
+    for(int i=1; i<=pos-1; i++){
+        tmp = tmp->next;
+    }
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+}
+
+int main(){
+    Node * head = NULL;
+    Node * tail = NULL;
+    while(1){
+        int val;
+        cin>>val;
+        if(val == -1) break;
+        insert_at_tail(head, tail, val);
+    }
+    int size = size_counter(head);
+    int v,p;
+    cin>>v>>p;
+    if(p>size){
+        cout<<"Invalid Position"<<endl;
+    }
+    else if(p == 0){
+        insert_at_head(head,v);
+    }
+    else{
+        insert_at_position(head, v, p);
+    }
+    print_linked_list(head);
+    
+    return 0;
+}
