e9b7da0e6fe8c608d51bfa32c6ac9b1dd391e153
Nayem Islam <128284940+zeeSaan25@users.noreply.github.com>
2023-12-11 01:26:46 +0600

    Add files via upload

---
+++ Module 7 : Singly Linked List Recap/Taking_input_in_linked_list.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define boost() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+class Node{
+        public:
+        int value;
+        Node* next;
+
+        Node(int value){
+            this->value=value;
+            this->next=NULL;
+        }
+    };
+
+void insert_at_tail(Node *& head, Node *& tail, int val){
+    Node * newNode = new Node(val);
+    if(head == NULL){
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void print_linked_list(Node * head){
+    Node * tmp = head;
+    while(tmp != NULL){
+        cout<<tmp->value<<" ";
+        tmp = tmp->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    boost();
+    Node * head = NULL;
+    Node * tail = NULL;
+    while(1){
+        int val;
+        cin>>val;
+        if(val == -1) break;
+        insert_at_tail(head, tail, val);
+    }
+
+    print_linked_list(head);
+    return 0;
+}
