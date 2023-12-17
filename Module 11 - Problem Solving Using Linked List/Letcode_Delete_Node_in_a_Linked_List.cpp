8bf0288535d3820d480eab731f4c43e0ac403373
Nayem Islam <128284940+zeeSaan25@users.noreply.github.com>
2023-12-17 17:09:43 +0600

    Update Letcode_Delete_Node_in_a_Linked_List.cpp

--- Module 11 - Problem Solving Using Linked List/Letcode_Delete_Node_in_a_Linked_List.cpp
+++ Module 11 - Problem Solving Using Linked List/Letcode_Delete_Node_in_a_Linked_List.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     void deleteNode(ListNode* node) {
         node->val = node->next->val;
+        ListNode * deleteNode = node->next;
         node->next = node->next->next;
+        delete deleteNode;
     }
 };
