a4aaf22bf10a009d0bc3a55b321d9a08d0c649f9
Nayem Islam <128284940+zeeSaan25@users.noreply.github.com>
2024-01-02 01:51:21 +0600

    Add files via upload

---
+++ Module 18 - Operations on Binary Tree/Level_Order_Traversal.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define boost() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+class Node{
+    public:
+    int val;
+    Node * left;
+    Node * right;
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void levelorder(Node * root){
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        Node * f = q.front();
+        q.pop();
+
+        cout<<f->val<<" ";
+        if(f->left != NULL){
+            q.push(f->left);
+        }
+        if(f->right != NULL){
+            q.push(f->right);
+        }
+    }
+}
+
+int main(){
+    boost();
+    Node * root = new Node(10);
+    Node * a = new Node(20);
+    Node * b = new Node(30);
+    Node * c = new Node(40);
+    Node * d = new Node(50);
+    Node * e = new Node(60);
+    Node * f = new Node(70);
+    Node * g = new Node(80);
+    Node * h = new Node(90);
+    Node * i = new Node(100);
+
+    //connection
+
+    root->left = a;
+    root->right = b;
+
+    a->left = c;
+    a->right = h;
+
+    c->right = e;
+
+    b->right = d;
+
+    d->left = f;
+    d->right = g;
+
+    h->right = i;
+    levelorder(root);
+    return 0;
+}
