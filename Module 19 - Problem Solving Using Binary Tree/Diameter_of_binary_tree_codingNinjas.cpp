9fbc84d7a33fe87411fe98ab0372eecebd5a3e06
Nayem Islam <128284940+zeeSaan25@users.noreply.github.com>
2024-01-05 15:58:24 +0600

    Add files via upload

---
+++ Module 19 - Problem Solving Using Binary Tree/Diameter_of_binary_tree_codingNinjas.cpp
@@ -0,0 +1,14 @@
+int mx=0;
+int maxHeight(TreeNode<int> * root){
+    if(root == NULL) return 0;
+    int l = maxHeight(root->left);
+    int r = maxHeight(root->right);
+    int d = l+r;
+    mx = max(mx,d);
+    return max(l,r)+1;
+}
+int diameterOfBinaryTree(TreeNode<int> *root){
+	int l = maxHeight(root->left);
+    int r = maxHeight(root->right);
+    return max(mx,l+r);
+}
